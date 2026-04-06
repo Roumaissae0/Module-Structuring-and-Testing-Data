688656bda355623b45a0df5e90522b0ab0b8c7cb
Roumaissae <alroumaissaebb@gmail.com>
2026-04-06 22:33:30 +0200

    I interpred the variables declarations

--- Sprint-1/3-mandatory-interpret/3-to-pounds.js
+++ Sprint-1/3-mandatory-interpret/3-to-pounds.js
@@ -25,3 +25,8 @@ console.log(`£${pounds}.${pence}`);
 
 // To begin, we can start with
 // 1. const penceString = "399p": initialises a string variable with the value "399p"
+
+// 2. const penceStringWithoutTrailingP = penceString.substring : it stars from 0 that is 3 and ends with penceString.length thats is all the text minus  the last letter that is p 
+// 3. const paddedPenceNumberString = penceStringWithoutTrailingP.padStart : we take the reponse of the line 3 and if is less than 3 characters we add 0 till is 3 character.
+// 4. const pounds = paddedPenceNumberString.substring : it takes only the 0 that is  the pound part and leaves the pences the las 2 characters. it separate them .
+// 5. const pence = paddedPenceNumberString : it does like the answer 4 but on the contrary it takes only the pences and leaves the pound .
