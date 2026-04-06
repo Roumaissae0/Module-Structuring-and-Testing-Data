3f8c96edc721d7ffc9bda0bf346311b5c3bd137f
Roumaissae <alroumaissaebb@gmail.com>
2026-04-06 22:07:25 +0200

    I answered all the questions and tried to change movielength

--- Sprint-1/3-mandatory-interpret/2-time-format.js
+++ Sprint-1/3-mandatory-interpret/2-time-format.js
@@ -1,4 +1,4 @@
-const movieLength = 8784; // length of movie in seconds
+const movieLength = 3600; // length of movie in seconds
 
 const remainingSeconds = movieLength % 60;
 const totalMinutes = (movieLength - remainingSeconds) / 60;
@@ -13,13 +13,25 @@ console.log(result);
 
 // a) How many variable declarations are there in this program?
 
+//    6
+
 // b) How many function calls are there?
+
+//    Only one console.log 
 
 // c) Using documentation, explain what the expression movieLength % 60 represents
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators
 
+//    MovieLength its whats left after sliping .
+
 // d) Interpret line 4, what does the expression assigned to totalMinutes mean?
+
+//    It means how many minutes there are after subtract totalminutes to remainingseconds and convert to minutes 
 
 // e) What do you think the variable result represents? Can you think of a better name for this variable?
 
+//    the hours minutes and seconds remaining of the movie . Maybe it can be RemainingOfMovie
+
 // f) Try experimenting with different values of movieLength. Will this code work for all values of movieLength? Explain your answer
+
+//    It works good but some times its weird the format only with one digit and not 2 like 1:0:0 it should be 01:01:01 but anyways it works perfect
