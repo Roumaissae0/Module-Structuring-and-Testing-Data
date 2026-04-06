97c61a47352b628d86f205711e10378dca7c7452
Roumaissae <alroumaissaebb@gmail.com>
2026-04-06 21:23:32 +0200

    I make it start with letter and not number because it only can start with letter .

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,8 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+const HourClockTime12 = "20:53";
+const hourClockTime24 = "08:53";
+
+console.log(HourClockTime12)
+console.log(hourClockTime24)
+
+// Tha variable name it cannot start with a number but it can contain a number 
+// Im gonna change the order of the value name 
