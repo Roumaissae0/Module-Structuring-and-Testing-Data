f9b4fe748d8e56ff3eb3d153f2bcaa35f5e860fe
Roumaissae <alroumaissaebb@gmail.com>
2026-04-06 21:10:14 +0200

    Ive changed the order that is written

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,5 +1,8 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
 
-console.log(`I was born in ${cityOfBirth}`);
+// The problem here is that we are making use of cityofbirth before that we define it we have to change the order so it works
+
+
 const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
