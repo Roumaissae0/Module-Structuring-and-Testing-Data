250c8ccd818bfe5433878ab1ba4323c8f1d8b670
Roumaissae <alroumaissaebb@gmail.com>
2026-04-06 20:58:35 +0200

    // Num represent a number minimum 1 and maximum 100 // Math.floor makes any number decimal go down like 1,99 the result is 1 not 2. // Math.random it gives me a random number between 1 and 100 // at the end add a + minimum (that is 1) it means that its not 1 to 99 its 1 to 100.

--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -7,3 +7,10 @@ const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 // Try breaking down the expression and using documentation to explain what it means
 // It will help to think about the order in which expressions are evaluated
 // Try logging the value of num and running the program several times to build an idea of what the program is doing
+
+// Num represent a number minimum 1 and maximum 100
+// Math.floor makes any number decimal go down like 1,99 the result is 1 not 2.
+// Math.random it gives me a random number between 1 and 100
+// at the end add a + minimum (that is 1) it means that its not 1 to 99 its 1 to 100.
+
+console.log(num);
