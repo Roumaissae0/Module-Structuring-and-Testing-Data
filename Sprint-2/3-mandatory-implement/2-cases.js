f997581f3061a80c570f7822829e67e418a102e4
Roumaissae <alroumaissaebb@gmail.com>
2026-04-19 21:50:09 +0200

    feat: add function to change text to upper snake case

--- Sprint-2/3-mandatory-implement/2-cases.js
+++ Sprint-2/3-mandatory-implement/2-cases.js
@@ -14,3 +14,11 @@
 // You will need to come up with an appropriate name for the function
 // Use the MDN string documentation to help you find a solution
 // This might help https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
+function toUpperSnakeCase(str){
+let wordWithUnderscores = str.replaceAll(" ", "_");
+
+let finalResult = wordWithUnderscores.toUpperCase();
+return finalResult;
+}
+console.log(toUpperSnakeCase("hello there")); 
+console.log(toUpperSnakeCase("lord of the rings")); 
